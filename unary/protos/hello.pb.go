162079c74c597e875c66ae4a2e2624ba289ec904
Harsh Thakur <harshthakur9030@gmail.com>
2020-11-25 01:57:42 +0530

    Third example
    Signed-off-by: Harsh Thakur <harshthakur9030@gmail.com>

--- unary/protos/hello.pb.go
+++ unary/protos/hello.pb.go
@@ -29,6 +29,44 @@ const (
 // of the legacy proto package is being used.
 const _ = proto.ProtoPackageIsVersion4
 
+type Null struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *Null) Reset() {
+	*x = Null{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_hello_proto_msgTypes[0]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Null) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Null) ProtoMessage() {}
+
+func (x *Null) ProtoReflect() protoreflect.Message {
+	mi := &file_hello_proto_msgTypes[0]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Null.ProtoReflect.Descriptor instead.
+func (*Null) Descriptor() ([]byte, []int) {
+	return file_hello_proto_rawDescGZIP(), []int{0}
+}
+
 type Person struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -42,7 +80,7 @@ type Person struct {
 func (x *Person) Reset() {
 	*x = Person{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_hello_proto_msgTypes[0]
+		mi := &file_hello_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -55,7 +93,7 @@ func (x *Person) String() string {
 func (*Person) ProtoMessage() {}
 
 func (x *Person) ProtoReflect() protoreflect.Message {
-	mi := &file_hello_proto_msgTypes[0]
+	mi := &file_hello_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -68,7 +106,7 @@ func (x *Person) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Person.ProtoReflect.Descriptor instead.
 func (*Person) Descriptor() ([]byte, []int) {
-	return file_hello_proto_rawDescGZIP(), []int{0}
+	return file_hello_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *Person) GetName() string {
@@ -92,58 +130,18 @@ func (x *Person) GetExist() bool {
 	return false
 }
 
-type Null struct {
-	state         protoimpl.MessageState
-	sizeCache     protoimpl.SizeCache
-	unknownFields protoimpl.UnknownFields
-}
-
-func (x *Null) Reset() {
-	*x = Null{}
-	if protoimpl.UnsafeEnabled {
-		mi := &file_hello_proto_msgTypes[1]
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		ms.StoreMessageInfo(mi)
-	}
-}
-
-func (x *Null) String() string {
-	return protoimpl.X.MessageStringOf(x)
-}
-
-func (*Null) ProtoMessage() {}
-
-func (x *Null) ProtoReflect() protoreflect.Message {
-	mi := &file_hello_proto_msgTypes[1]
-	if protoimpl.UnsafeEnabled && x != nil {
-		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
-		if ms.LoadMessageInfo() == nil {
-			ms.StoreMessageInfo(mi)
-		}
-		return ms
-	}
-	return mi.MessageOf(x)
-}
-
-// Deprecated: Use Null.ProtoReflect.Descriptor instead.
-func (*Null) Descriptor() ([]byte, []int) {
-	return file_hello_proto_rawDescGZIP(), []int{1}
-}
-
 var File_hello_proto protoreflect.FileDescriptor
 
 var file_hello_proto_rawDesc = []byte{
-	0x0a, 0x0b, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x48, 0x0a,
-	0x06, 0x50, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x12, 0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18,
-	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x6e,
-	0x61, 0x6d, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03, 0x28, 0x09, 0x52, 0x05, 0x6e, 0x61, 0x6d, 0x65,
-	0x73, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x78, 0x69, 0x73, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x08,
-	0x52, 0x05, 0x65, 0x78, 0x69, 0x73, 0x74, 0x22, 0x06, 0x0a, 0x04, 0x4e, 0x75, 0x6c, 0x6c, 0x32,
-	0x22, 0x0a, 0x07, 0x57, 0x65, 0x6c, 0x63, 0x6f, 0x6d, 0x65, 0x12, 0x17, 0x0a, 0x05, 0x57, 0x6f,
-	0x72, 0x6c, 0x64, 0x12, 0x05, 0x2e, 0x4e, 0x75, 0x6c, 0x6c, 0x1a, 0x07, 0x2e, 0x50, 0x65, 0x72,
-	0x73, 0x6f, 0x6e, 0x32, 0x22, 0x0a, 0x06, 0x4c, 0x6f, 0x6f, 0x6b, 0x75, 0x70, 0x12, 0x18, 0x0a,
-	0x04, 0x46, 0x69, 0x6e, 0x64, 0x12, 0x07, 0x2e, 0x50, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x1a, 0x07,
-	0x2e, 0x50, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x0a, 0x0b, 0x68, 0x65, 0x6c, 0x6c, 0x6f, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x06, 0x0a,
+	0x04, 0x4e, 0x75, 0x6c, 0x6c, 0x22, 0x48, 0x0a, 0x06, 0x50, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x12,
+	0x12, 0x0a, 0x04, 0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e,
+	0x61, 0x6d, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x6e, 0x61, 0x6d, 0x65, 0x73, 0x18, 0x02, 0x20, 0x03,
+	0x28, 0x09, 0x52, 0x05, 0x6e, 0x61, 0x6d, 0x65, 0x73, 0x12, 0x14, 0x0a, 0x05, 0x65, 0x78, 0x69,
+	0x73, 0x74, 0x18, 0x03, 0x20, 0x01, 0x28, 0x08, 0x52, 0x05, 0x65, 0x78, 0x69, 0x73, 0x74, 0x32,
+	0x22, 0x0a, 0x06, 0x4c, 0x6f, 0x6f, 0x6b, 0x75, 0x70, 0x12, 0x18, 0x0a, 0x04, 0x46, 0x69, 0x6e,
+	0x64, 0x12, 0x07, 0x2e, 0x50, 0x65, 0x72, 0x73, 0x6f, 0x6e, 0x1a, 0x07, 0x2e, 0x50, 0x65, 0x72,
+	0x73, 0x6f, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -160,16 +158,14 @@ func file_hello_proto_rawDescGZIP() []byte {
 
 var file_hello_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_hello_proto_goTypes = []interface{}{
-	(*Person)(nil), // 0: Person
-	(*Null)(nil),   // 1: Null
+	(*Null)(nil),   // 0: Null
+	(*Person)(nil), // 1: Person
 }
 var file_hello_proto_depIdxs = []int32{
-	1, // 0: Welcome.World:input_type -> Null
-	0, // 1: Lookup.Find:input_type -> Person
-	0, // 2: Welcome.World:output_type -> Person
-	0, // 3: Lookup.Find:output_type -> Person
-	2, // [2:4] is the sub-list for method output_type
-	0, // [0:2] is the sub-list for method input_type
+	1, // 0: Lookup.Find:input_type -> Person
+	1, // 1: Lookup.Find:output_type -> Person
+	1, // [1:2] is the sub-list for method output_type
+	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
@@ -182,7 +178,7 @@ func file_hello_proto_init() {
 	}
 	if !protoimpl.UnsafeEnabled {
 		file_hello_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Person); i {
+			switch v := v.(*Null); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -194,7 +190,7 @@ func file_hello_proto_init() {
 			}
 		}
 		file_hello_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*Null); i {
+			switch v := v.(*Person); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -214,7 +210,7 @@ func file_hello_proto_init() {
 			NumEnums:      0,
 			NumMessages:   2,
 			NumExtensions: 0,
-			NumServices:   2,
+			NumServices:   1,
 		},
 		GoTypes:           file_hello_proto_goTypes,
 		DependencyIndexes: file_hello_proto_depIdxs,
@@ -233,78 +229,6 @@ var _ grpc.ClientConnInterface
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion6
-
-// WelcomeClient is the client API for Welcome service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type WelcomeClient interface {
-	World(ctx context.Context, in *Null, opts ...grpc.CallOption) (*Person, error)
-}
-
-type welcomeClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewWelcomeClient(cc grpc.ClientConnInterface) WelcomeClient {
-	return &welcomeClient{cc}
-}
-
-func (c *welcomeClient) World(ctx context.Context, in *Null, opts ...grpc.CallOption) (*Person, error) {
-	out := new(Person)
-	err := c.cc.Invoke(ctx, "/Welcome/World", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// WelcomeServer is the server API for Welcome service.
-type WelcomeServer interface {
-	World(context.Context, *Null) (*Person, error)
-}
-
-// UnimplementedWelcomeServer can be embedded to have forward compatible implementations.
-type UnimplementedWelcomeServer struct {
-}
-
-func (*UnimplementedWelcomeServer) World(context.Context, *Null) (*Person, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method World not implemented")
-}
-
-func RegisterWelcomeServer(s *grpc.Server, srv WelcomeServer) {
-	s.RegisterService(&_Welcome_serviceDesc, srv)
-}
-
-func _Welcome_World_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Null)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(WelcomeServer).World(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/Welcome/World",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WelcomeServer).World(ctx, req.(*Null))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _Welcome_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "Welcome",
-	HandlerType: (*WelcomeServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "World",
-			Handler:    _Welcome_World_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "hello.proto",
-}
 
 // LookupClient is the client API for Lookup service.
 //
--- unary/server.go
+++ unary/server.go
@@ -24,9 +24,6 @@ func main() {
 	// This helps clients determine which services are available to call( like robots.txt but for gRPC)
 	reflection.Register(gs)
 
-	// Register the name of the service to the server
-	protos.RegisterWelcomeServer(gs, &welcome{})
-
 	protos.RegisterLookupServer(gs, &lookup{})
 
 	l, err := net.Listen("tcp", "0.0.0.0:9090")
@@ -39,82 +36,23 @@ func main() {
 	gs.Serve(l)
 }
 
-type welcome struct {
-}
-
-// // First Example
-// func (w *welcome) World(ctx context.Context, null *protos.Null) (*protos.Person, error) {
-
-// 	name := "CNCF and Hashicorp User group"
-
-// 	return &protos.Person{
-// 		Name: name,
-// 	}, nil
-// }
-
-// Second example
-func (w *welcome) World(ctx context.Context, null *protos.Null) (*protos.Person, error) {
-
-	name := "CNCF and Hashicorp User group"
-
-	names := make([]string, 0)
-
-	names = append(names, "CNCF")
-
-	names = append(names, "Hashicorp")
-
-	return &protos.Person{
-		Name:  name,
-		Names: names,
-	}, nil
-}
-
 type lookup struct {
 }
 
 // Third example
-// func (l *lookup) Find(ctx context.Context, p *protos.Person) (*protos.Person, error) {
-// 	names := make([]string, 0)
-// 	var exist bool
-
-// 	names = append(names, "Hashicorp")
-// 	names = append(names, "CNCF")
-
-// 	for _, n := range names {
-// 		if n == p.Name {
-// 			exist = true
-// 		}
-// 		if p.GetName() == "grpc" {
-// 			return nil, status.Errorf(codes.InvalidArgument, "Do not send grpc here")
-// 		}
-// 	}
-
-// 	return &protos.Person{
-// 		Exist: exist,
-// 	}, nil
-// }
-
-// Fourth example
 func (l *lookup) Find(ctx context.Context, p *protos.Person) (*protos.Person, error) {
 	names := make([]string, 0)
 	var exist bool
 
-	names = append(names, "Hashicorp")
 	names = append(names, "CNCF")
+	names = append(names, "Meetup")
 
 	for _, n := range names {
 		if n == p.Name {
 			exist = true
 		}
 		if p.GetName() == "grpc" {
-			err := status.Newf(codes.InvalidArgument, "Don't send gRPC as data")
-			err, yae := err.WithDetails(p)
-			if yae != nil {
-				return nil, yae
-
-			}
-			return nil, err.Err()
-			// return nil, status.Errorf(codes.InvalidArgument, "Do not send grpc here")
+			return nil, status.Errorf(codes.InvalidArgument, "Do not send grpc here")
 		}
 	}
 
